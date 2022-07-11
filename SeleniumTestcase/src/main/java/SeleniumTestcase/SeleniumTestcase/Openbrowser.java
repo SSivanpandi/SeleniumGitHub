8d33075be19a547d60c65ae76385cb16bc081aea
Sugumar Sivanpandi <sugumar.sivanpandi007@gmail.com>
2022-07-11 23:14:51 +0100

    Initial Commit

---
+++ SeleniumTestcase/src/main/java/SeleniumTestcase/SeleniumTestcase/Openbrowser.java
@@ -0,0 +1,27 @@
+package SeleniumTestcase.SeleniumTestcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class Openbrowser
+{
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Sugumar Sivanpandi\\Desktop\\Chromedriver\\chromedriver.exe");
+
+        ExtentSparkReporter Spark = new ExtentSparkReporter("target/Reports.html");
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(Spark);
+        extent.createTest("Whatsapp").pass("Testcase Passed");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        driver.findElement(By.id("L2AGLb")) .click();
+        driver.findElement(By.name("q")).sendKeys("Whatsapp");
+        driver.findElement(By.name("q")).submit();
+        driver.quit();
+        extent.flush();
+    }
+}
